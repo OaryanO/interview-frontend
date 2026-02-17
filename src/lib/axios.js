c4f6871dcb13b12d2b683c7d46244786dc9b49af
Aryan Singh <97840303+OaryanO@users.noreply.github.com>
2026-02-17 22:27:36 +0530

    axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,31 @@
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+//   withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
+// });
+
+// export default axiosInstance;
+
 import axios from "axios";
+import { useAuth } from "@clerk/clerk-react";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
 });
 
-export default axiosInstance;
+export const useAxios = () => {
+  const { getToken } = useAuth();
+
+  axiosInstance.interceptors.request.use(async (config) => {
+    const token = await getToken();
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  });
+
+  return axiosInstance;
+};
